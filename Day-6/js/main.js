f3f4532868a68a9001798e5e3173807a952b3d92
Nirmal Chandra Hota <raju.mahato@utkallabs.com>
2023-07-03 21:59:39 +0530

    We cover fundamental of JS

---
+++ Day-6/js/main.js
@@ -0,0 +1,83 @@
+// let item1 = document.getElementsByClassName('item-1');
+
+// let toggle = false;
+
+// let myFunc = () => {
+//     toggle = !toggle
+//     if(toggle) {
+//         item1[0].style.backgroundColor = "red";
+//     } else {
+//         item1[0].style.backgroundColor = "darkmagenta";
+//     }
+// }
+
+// item1[0].addEventListener('click', myFunc);
+
+
+// Variable and DataTypes  -- Done
+// Condition (if/else)
+// Javascript Function
+// Operator (Arithmatic, Assignment, logical and etc)
+
+
+// Javascript Event
+
+// DOM manipulation
+
+// Variables and DataTypes
+
+// Variable declaration
+
+// const x = 300 /// 1001
+// const y = 200;
+// const sum = x + y;
+
+// console.log(sum);
+// console.log(x);
+// console.log(x);
+// console.log(x);
+// console.log(x + sum);
+
+
+// DataTypes
+// Number
+// String
+// Boolean
+
+// let x = 'qwe';
+
+// let z = x * x;
+
+// console.log(x * x);
+
+// console.log(typeof x);
+
+// Condition
+// 1. if and else 
+// 2. switch case
+
+// 60 - First 
+
+// 50 - second
+
+// 40 - third
+
+// below - fail
+
+let mark = 80;
+
+if(mark > 60) {
+    console.log("First");
+} else if(mark > 50) {
+    console.log("Second");
+} else if (mark > 40) {
+    console.log("Third");
+} else {
+    console.log("Fail")
+}
+
+console.log("End")
+
+
+
+
